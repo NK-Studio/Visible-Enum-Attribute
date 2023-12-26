45c5c4d0043e76966686a1eba7c0a2179c92f0cf
NK Studio <53283888+NK-Studio@users.noreply.github.com>
2023-12-26 09:12:18 +0900

    Read Me 수정

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Visible-Enum-Event-Attribute
+# Visible Enum Attribute
+![image01.png](Image%2Fimage01.png)
+유니티의 Enum 타입 변수를 인스펙터에서 선택 가능하게 만들어주는 Attribute입니다.
+
+## 사용법
+```cs
+public enum EFruit
+{
+    Apple,
+    Banana,
+    Orange,
+    Pear,
+}
+
+public class TestCode : MonoBehaviour
+{
+    [VisibleEnum(typeof(EFruit))]
+    public void OnChangeFruit(int fruit)
+    {
+        EFruit fruitEnum = (EFruit)fruit;
+        Debug.Log($"Fruit : {fruitEnum}");
+    }
+}
+```
+
--- Runtime/VisibleEnumAttribute.cs
+++ Runtime/VisibleEnumAttribute.cs
@@ -1,9 +1,12 @@
 using System;
 using UnityEngine;
 
-[AttributeUsage(AttributeTargets.Method)]
-public class VisibleEnumAttribute : PropertyAttribute
+namespace UnityEngine
 {
-    public Type EnumType { get; }
-    public VisibleEnumAttribute(Type enumType) => EnumType = enumType;
+    [AttributeUsage(AttributeTargets.Method)]
+    public class VisibleEnumAttribute : PropertyAttribute
+    {
+        public Type EnumType { get; }
+        public VisibleEnumAttribute(Type enumType) => EnumType = enumType;
+    }
 }
